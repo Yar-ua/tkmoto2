35d6066b09cb3d25dddde910f22e8cf16dacc69e
Ubuntu <ubuntu@ip-172-31-9-48.eu-central-1.compute.internal>
2020-09-24 11:51:25 +0000

    refactoring smth in fuels

--- app/models/fuel.rb
+++ app/models/fuel.rb
@@ -1,7 +1,9 @@
 class Fuel < ApplicationRecord
   belongs_to :bike
   
-  # validates		:odo, :odo_delta, :refueling, :price_fuel, presence: true
-  # validates		:odo, :odo_delta, :refueling, :price_fuel, numericality: true
+  validates		:odo, presence: true, length: { maximum: 50 }, numericality: true
+  validates   :odo_delta, presence: true, length: { maximum: 50 }, numericality: true
+  validates   :refueling, presence: true, length: { maximum: 50 }, numericality: true
+  validates   :price_fuel, presence: true, length: { maximum: 50 }, numericality: true
 
 end
